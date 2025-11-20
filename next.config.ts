0302450e633442a107c4dfe81c58912d3ef598fd
khkiiim01 <khkiiim01@gmail.com>
2025-11-20 15:28:02 +0900

    rollback

--- next.config.ts
+++ next.config.ts
@@ -2,28 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  
-  async redirects() {
-  return [
-    {
-      source: "/result",
-      has: [
-        {
-          type: "query",
-          key: "websiteId",
-        },
-      ],
-      destination: "/result",
-      permanent: false,
-    },
-  ];
-  },
 
   async rewrites() {
     return [
       {
         source: "/api-proxy/:path*",
-        destination: "http://15.164.29.199:8080/:path*", 
+        destination: "http://15.164.29.199:8080/:path*",
       },
     ];
   },
